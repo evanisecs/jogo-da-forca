6c8960010fcb71dee27d4e05acf0b1030aa9bb35
Evanise C Silva <evanisecs@gmail.com>
2024-03-17 01:33:47 -0300

    FEATURE - jogo da forca finalizado

--- asset/script/script.js
+++ asset/script/script.js
@@ -3,21 +3,37 @@ const wordDisplay = document.querySelector(".word-display");
 const guessesText = document.querySelector(".guesses-text b");
 const keyboardDiv = document.querySelector(".keyboard");
 const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = document.querySelector(".play-again");
 
-let currentWord, correctLetters = [], wrongGuessCount = 0;
+let currentWord, correctLetters, wrongGuessCount;
 const maxGuesses = 6;
+
+const resetGame = () => {
+    // Ressetting all game variables and UI elements
+    correctLetters = [];
+    wrongGuessCount = 0;
+    hangmanImage.src = `asset/hangman-game-images/images/hangman-${wrongGuessCount}.svg`;
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");          
+    gameModal.classList.remove("show");
+}
 
 const getRandomWord = () => {
     //Selecting a random word and hint from  the  wordlist
     const{ word, hint} = wordlist[Math.floor(Math.random() * wordlist.length)];
     currentWord = word;
-    console.log(word);
     document.querySelector(".hint-text b").innerText = hint;
-    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
-}
-
+    resetGame();    
+}   
+    
 const gameOver =(isVictory) => {
+    //After 600ms of game complete.. showing modal with relevant details
     setTimeout(() =>{
+        const modaltext = isVictory ? `You found the word:` : `The correct word was:`;
+        gameModal.querySelector("img").src = `asset/hangman-game-images/images/${isVictory ? 'victory' : 'lost'}.gif`;
+        gameModal.querySelector("h4").innerText = `${isVictory ? 'Congrats!' : 'Game Over!'}`;
+        gameModal.querySelector("p").innerHTML = `${modaltext} <b>${currentWord}</b>`;
         gameModal.classList.add("show");
     }, 300)
 }
@@ -57,3 +73,4 @@ for (let i = 97; i <= 122; i++) {
 }
 
 getRandomWord();
+playAgainBtn.addEventListener("click", getRandomWord);
